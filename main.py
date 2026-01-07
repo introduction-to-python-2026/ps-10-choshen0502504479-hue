8f39c93235fefe9be9321ed99afee820819db6ef
choshen0502504479-hue <choshen0502504479@gmail.com>
2026-01-07 23:32:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+from skimage.filters import median
+from skimage.morphology import ball
+from image_utils import load_image , edge_detection
+ 
+original_image = load_image('/content/mada.jpeg')
+clean_image = median(original_image, ball(3))
+edge_mag = edge_detection(clean_image)
+threshold_value = 100 
+binary_edges = edge_mag > threshold_value
+
+edge_image_to_save = Image.fromarray((binary_edges * 255).astype(np.uint8))
+edge_image_to_save.save('my_edges.png')
 
